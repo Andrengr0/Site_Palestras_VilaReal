7e32956befaf4ad8754049e460ae52d944768e50
André Negreiros <112659872+Andrengr0@users.noreply.github.com>
2023-09-01 21:28:31 -0300

    add 01/09

--- js/functions.js
+++ js/functions.js
@@ -30,7 +30,7 @@ $(()=>{
     })
     
     // Funções para dar o efeito hover em cada opção do menu conforme o scroll vai rolando
-    $('.id-inicio').css('border-bottom','3px solid #ffffff');
+    $('.id-inicio').css('border-bottom','2px solid #ffffff');
     $(window).scroll(function(){
         var windowOffY = $(window).scrollTop();
         var windowHeight = $(window).height();
@@ -41,15 +41,15 @@ $(()=>{
             if(elOffY-350 < windowOffY && windowOffY > windowHeight){
                 $('header a, header-2 a').css('border','0');
                 var target = $(this).attr('target');
-                $('.'+target).css('border-bottom','3px solid #ffffff');
+                $('.'+target).css('border-bottom','2px solid #ffffff');
                 return;
             }else if(windowOffY <= windowHeight){
-                $('.id-inicio').css('border-bottom','3px solid #ffffff');
+                $('.id-inicio').css('border-bottom','2px solid #ffffff');
                 $('.id-palestras').css('border','0');
                 return;
             }else if(elOffContato < window){
                 $('header a').css('border','0');
-                $('.id-contato').css('border-bottom','3px solid #ffffff');
+                $('.id-contato').css('border-bottom','2px solid #ffffff');
                 $('.id-quem-somos').css('border','0');
             }   
         }) 
